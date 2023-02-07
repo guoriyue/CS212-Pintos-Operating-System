9d37f51ab2b065fee708e262d01e3f5202a3ddbf
cate2002 <91645354+cate2002@users.noreply.github.com>
2023-02-06 17:05:11 -0800

    Update syscall.h

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -10,5 +10,13 @@ void sysexit (int status);
 int syswait (int pid);
 int sysopen (const char *file, uint8_t *esp);
 int syswrite (int fd, const void * buffer, unsigned size, uint8_t *esp);
+void halt (void);
+bool create (const char *file, unsigned initial_size, uint8_t *esp);
+bool remove (const char *file);
+int filesize (int fd);
+int read (int fd, void *buffer, unsigned size);
+void seek (int fd, unsigned position);
+unsigned tell (int fd);
+void close (int fd);
 
 #endif /* userprog/syscall.h */
