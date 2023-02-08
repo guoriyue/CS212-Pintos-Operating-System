d8e1ba26ac1ea1a1cc3ba63a6e6189d7c777fba8
cate2002 <91645354+cate2002@users.noreply.github.com>
2023-02-07 21:31:09 -0800

    syscall.h

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -3,6 +3,8 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+#include "threads/thread.h"
+
 void syscall_init (void);
 
 bool valid_user_pointer (void* user_pointer, unsigned size);
@@ -10,13 +12,14 @@ void sysexit (int status);
 int syswait (int pid);
 int sysopen (const char *file, uint8_t *esp);
 int syswrite (int fd, const void * buffer, unsigned size, uint8_t *esp);
-void halt (void);
-bool create (const char *file, unsigned initial_size, uint8_t *esp);
-bool remove (const char *file);
-int filesize (int fd);
-int read (int fd, void *buffer, unsigned size);
-void seek (int fd, unsigned position);
-unsigned tell (int fd);
-void close (int fd);
+void syshalt (void);
+bool syscreate (const char *file, unsigned initial_size, uint8_t *esp);
+bool sysremove (const char *file);
+int sysfilesize (int fd);
+int sysread (int fd, void *buffer, unsigned size);
+void sysseek (int fd, unsigned position);
+unsigned systell (int fd);
+void sysclose (int fd);
+tid_t sysexec (const char * cmd_line);
 
 #endif /* userprog/syscall.h */
