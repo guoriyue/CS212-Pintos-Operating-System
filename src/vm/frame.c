59fb7e030f14311477f276813b9e25b01e630b87
cate2002 <91645354+cate2002@users.noreply.github.com>
2023-03-02 22:26:18 -0800

    Update frame.c

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -8,15 +8,7 @@
 #include <stdbool.h>
 #include "threads/malloc.h"
 #include "threads/synch.h"
-<<<<<<< HEAD
-#include "vm/page.h"
 // #include "userporg/process.h"
-
-uint32_t clock = 0;
-
-=======
-// #include "userporg/process.h"
->>>>>>> 45860a9dc820e02aa24dc9bdb7c15b0e081ccb8f
 bool
 frame_table_empty (struct frame_table *f, size_t start, size_t cnt) 
 {
@@ -78,97 +70,10 @@ frame_table_init (size_t frame_table_entry_number, uint8_t* frame_table_base)
     lock_init (&(frame_table->frame_table_entry[i].frame_lock));
     frame_table->frame_table_entry[i].frame = (uint32_t*) (frame_table->base + (i << 12));
   }
-<<<<<<< HEAD
-  lock_init (&(frame_table->frame_table_lock));
-=======
->>>>>>> 45860a9dc820e02aa24dc9bdb7c15b0e081ccb8f
 }
 
 uint32_t
 frame_table_get_id (void* kaddr)
 {
   return ((uint32_t) kaddr - (uint32_t) frame_table->base) >> 12;
-<<<<<<< HEAD
-}
-
-void
-frame_table_entry_free (struct supplementary_page_table_entry* spte) {
-  // no frame at the beginning
-  if (spte->fid == NULL)
-  {
-    return ;
-  }
-  struct frame_table_entry fte = frame_table->frame_table_entry[(uint32_t) spte->fid];
-  lock_acquire (&fte.frame_lock);
-  // the frame is still occupied
-  if (fte.spte->pid == spte->pid)
-  {
-    spte->fid = NULL;
-    fte.spte = NULL;
-    palloc_free_page(fte.frame);
-  }
-  lock_release (&fte.frame_lock);
-}
-
-
-void clock_algorithm (void)
-{
-  clock += 1;
-  clock = clock % frame_table->frame_table_entry_number;
-}
-
-struct frame_table_entry* frame_table_evict (void) {
-  // clock_algorithm ();
-  struct frame_table_entry fte;
-  while (true)
-  {
-    fte = frame_table->frame_table_entry[clock];
-    if (!lock_held_by_current_thread (&fte.frame_lock))
-    {
-      if (lock_try_acquire (&fte.frame_lock))
-      {
-        lock_acquire (&fte.spte->page_lock);
-        uint32_t* pagedir = fte.spte->pagedir;
-        // Returns true if page directory pd contains a page table entry for page that is marked dirty (or accessed). Otherwise, returns false.
-        bool accessed = pagedir_is_accessed (pagedir, fte.spte->pid);
-        // If page directory pd has a page table entry for page, then its dirty (or accessed) bit is set to value.
-        pagedir_set_accessed (pagedir, fte.spte->pid, false);
-        lock_release (&fte.spte->page_lock);
-        if (accessed) {
-          lock_release(&fte.frame_lock);
-        } else {
-          lock_acquire (&fte.spte->page_lock);
-
-          // lock_acquire(&fte.spte->page_lock);
-          pagedir_clear_page (fte.spte->pagedir, fte.spte->pid);
-          // lock_release(&fte.spte->page_lock);
-
-          if (fte.spte->location == MAP_MEMORY)
-          {
-            evict_page_map (fte.spte);
-          }
-          else if (fte.spte->location == FILE_SYSTEM)
-          {
-            evict_page_file (fte.spte);
-          }
-          else if (fte.spte->location == SWAP_BLOCK)
-          {
-            evict_page_swap (fte.spte);
-          }
-          else
-          {
-            sysexit (-1);
-          }
-          lock_release (&fte.spte->page_lock);
-          fte.spte = NULL;
-
-          return &fte;
-        }
-      }
-    }
-    clock_algorithm ();
-  }
-  return NULL;
-=======
->>>>>>> 45860a9dc820e02aa24dc9bdb7c15b0e081ccb8f
 }
