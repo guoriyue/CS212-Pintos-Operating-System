403f994438769da69eafc05f1857454101c11f1a
cate2002 <91645354+cate2002@users.noreply.github.com>
2023-03-02 22:27:13 -0800

    Update page.h

--- src/vm/page.h
+++ src/vm/page.h
@@ -4,43 +4,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include <list.h>
-/* Need typedef and repeat name. */
-typedef enum page_location
-{
-    MAP_MEMORY,
-    // in memory mapped
-    SWAP_BLOCK,
-    // in swap block
-    FILE_SYSTEM,
-    // in file system
-} page_location;
-
-struct supplementary_page_table_entry
-{
-    struct lock page_lock;
-    page_location location;
-    void* pid;
-    void* fid;
-<<<<<<< HEAD
-    void* sid;
-=======
->>>>>>> 45860a9dc820e02aa24dc9bdb7c15b0e081ccb8f
-    bool writable;
-    struct list_elem supplementary_page_table_entry_elem;
-    // uint32_t *pte;
-
-    size_t page_read_bytes;
-    size_t page_zero_bytes;
-
-    struct file *file;
-<<<<<<< HEAD
-    int32_t file_ofs; // int32_t
-    uint32_t **pagedir;
-=======
-    off_t file_ofs;
->>>>>>> 45860a9dc820e02aa24dc9bdb7c15b0e081ccb8f
-};
+#include "threads/synch.h"
+#include "threads/thread.h"
+#include "threads/pte.h"
+#include "threads/palloc.h"
+#include "vm/page.h"
+#include "vm/frame.h"
+#include "filesys/file.h"
+#include "userprog/syscall.h"
+#include "userprog/pagedir.h"
 
 struct supplementary_page_table_entry* supplementary_page_table_entry_create
 (void* pid,
@@ -49,28 +21,191 @@ page_location location,
 size_t page_read_bytes,
 size_t page_zero_bytes,
 struct file* file,
-<<<<<<< HEAD
-int32_t file_ofs);
-=======
-off_t file_ofs);
->>>>>>> 45860a9dc820e02aa24dc9bdb7c15b0e081ccb8f
+off_t file_ofs,
+struct thread *thread)
+{
+  struct supplementary_page_table_entry* spte = malloc (sizeof(struct supplementary_page_table_entry));
+  lock_init (&spte->page_lock);
+  
+  spte->pid = pid;
+  spte->writable = writable;
+  spte->location = location;
+  spte->mmap_dirty = false;
+  spte->thread = thread_current();
 
-void supplementary_page_table_entry_insert (struct supplementary_page_table_entry* e);
-void load_page_from_swap_block (struct supplementary_page_table_entry* spte);
-void load_page_from_map_memory (struct supplementary_page_table_entry* spte);
-void load_page_from_file_system (struct supplementary_page_table_entry* spte);
+  spte->page_read_bytes = page_read_bytes;
+  spte->page_zero_bytes = page_zero_bytes;
+
+  spte->file = file;
+  spte->file_ofs = file_ofs;
+
+  return spte;
+}
+
+void supplementary_page_table_entry_insert (struct supplementary_page_table_entry* spte)
+{
+  lock_acquire (&spte->page_lock);
+  list_push_back (&thread_current ()->supplementary_page_table, &spte->supplementary_page_table_entry_elem);
+  lock_release (&spte->page_lock);
+}
+
 struct supplementary_page_table_entry* supplementary_page_table_entry_find
-(void* vaddr);
+(void* vaddr)
+{
+  struct thread* cur = thread_current ();
+  void* pid = pg_round_down (vaddr);
+  struct list_elem *e;
+  for (e = list_begin (&cur->supplementary_page_table); e != list_end (&cur->supplementary_page_table);
+      e = list_next (e))
+  {
+    struct supplementary_page_table_entry* spte = list_entry (e, struct supplementary_page_table_entry, supplementary_page_table_entry_elem);
+    if (spte->pid == pid) {
+      return spte;
+    }
+  }
+  return NULL;
+}
 
+void clear_supplementary_page_table (void)
+{
+  struct thread* cur = thread_current ();
+  struct list_elem *e;
+  for (e = list_begin (&cur->supplementary_page_table); e != list_end (&cur->supplementary_page_table);
+      e = list_next (e))
+  {
+    struct supplementary_page_table_entry* spte = list_entry (e, struct supplementary_page_table_entry, supplementary_page_table_entry_elem);
+    if (spte->location == MAP_MEMORY) {
+      mapid_t mapping = (int)spte->pid >> 12;
+      sysmunmap(mapping);
+    }
+  }
+}
+
+void load_page_from_map_memory (struct supplementary_page_table_entry* spte)
+{
+  // void* kaddr = palloc_get_page (PAL_USER);
+  // spte->fid = frame_table_get_id (kaddr);
+  // struct frame_table_entry fte = frame_table->frame_table_entry[(uint32_t) spte->fid];
+  load_page_from_file_system (spte);
+}
+
+void load_page_from_swap_block (struct supplementary_page_table_entry* spte)
+{
+  void* kaddr = palloc_get_page (PAL_USER);
+  spte->fid = (void*) frame_table_get_id (kaddr);
+  struct frame_table_entry fte = frame_table->frame_table_entry[(uint32_t) spte->fid];
+}
+
+void load_page_from_file_system (struct supplementary_page_table_entry* spte)
+{
+  /* Kernel address. */
+  void* kaddr = palloc_get_page (PAL_USER);
+  struct frame_table_entry fte;
+
+  if (kaddr)
+  {
+    // printf ("valid kaddr\n");
+    spte->fid = (void*) frame_table_get_id (kaddr);
+    // Obtain a frame to store the page.
+    fte = frame_table->frame_table_entry[(uint32_t) spte->fid];
+    // printf ("frame id %d\n", (uint32_t) spte->fid);
+  }
+  else
+  {
+    printf ("eviction need!\n");
+    // fte = frame_table_evict ();
+  }
+
+  /* All zero page. */
+  if (spte->page_zero_bytes == PGSIZE)
+  {
+    // printf ("all zero\n");
+    memset (fte.frame, 0, PGSIZE);
+    if (install_page_copy (spte->pid, fte.frame, spte->writable))
+    {
+      // printf ("++++1success map\n");
+    }
+    return ;
+  }
+
+  
+  // Fetch data into the frame.
+  uint32_t read_bytes = file_read_at (spte->file, fte.frame, spte->page_read_bytes, spte->file_ofs);
+
+  if (spte->page_read_bytes != PGSIZE)
+  {
+    memset (fte.frame + spte->page_read_bytes, 0, PGSIZE - spte->page_read_bytes);
+  }
+  // printf ("after read\n");
+  if (read_bytes != spte->page_read_bytes)
+  {
+    // thread_exit();
+    palloc_free_page (kaddr);
+    sysexit (-1);
+  }
+
+  // // Update the page table entry to point the virtual address to the new 
+  // // physical address
+  // struct thread *cur = thread_current ();
+  // bool success = false;
+  // if (pagedir_get_page (cur->pagedir, spte->pid) == NULL)
+  // {
+  //   if (pagedir_set_page (cur->pagedir, spte->pid, fte.frame, spte->writable))
+  //   {
+  //     success = true;
+  //   }
+  // }
+
+  if (install_page_copy (spte->pid, fte.frame, spte->writable))
+  {
+    // printf ("success map\n");
+    // printf ("++++2success map\n");
+  }
+  else
+  {
+    palloc_free_page (kaddr);
+    sysexit (-1);
+    return ;
+  }
+}
+
+
+
+/* Have some problem to include process.h.
+   So just copy install_page here. */
+/* If static, can't be called in other classes. (Why?) */
 bool
-install_page_copy (void *upage, void *kpage, bool writable);
-<<<<<<< HEAD
+install_page_copy (void *upage, void *kpage, bool writable)
+{
+  struct thread *t = thread_current ();
 
-void
-evict_page_map(struct supplementary_page_table_entry* spte);
-void 
-evict_page_file (struct supplementary_page_table_entry* spte);
-void
-evict_page_swap (struct supplementary_page_table_entry* spte);
-=======
->>>>>>> 45860a9dc820e02aa24dc9bdb7c15b0e081ccb8f
+  /* Verify that there's not already a page at that virtual
+     address, then map our page there. */
+  return (pagedir_get_page (t->pagedir, upage) == NULL
+          && pagedir_set_page (t->pagedir, upage, kpage, writable));
+}
+
+
+bool supplementary_page_table_entry_find_between
+(void *start, void *end)
+{
+    struct thread* cur = thread_current ();
+    //void* pid_target = pg_round_down (start);
+    //void* pid_end = pg_round_down (end);
+    struct list_elem *e;
+    for (e = list_begin (&cur->supplementary_page_table); e != list_end (&cur->supplementary_page_table);
+        e = list_next (e))
+    {
+      struct supplementary_page_table_entry* spte = list_entry (e, struct supplementary_page_table_entry, supplementary_page_table_entry_elem);
+      int spte_size = file_length(spte->file);
+      if ((end >= spte->pid) && (end <= (void *)((int)spte->pid + spte_size))) {
+        return true;
+      }
+
+      if ((start >= spte->pid) && (start <= (void *)((int)spte->pid + spte_size))) {
+        return true;
+      }
+    }
+    return false;
+}
+
